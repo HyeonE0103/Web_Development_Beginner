6dfbcc024de1d05a861eaa4194cc5fe004d0828c
Sohyeon <mag30102000@gmail.com>
2022-12-01 01:08:22 +0900

    3주차_서버만들기

---
+++ Week_3/test.py
@@ -0,0 +1,17 @@
+number = 100
+num = 10
+n= 1
+data = [1, 2, 3, 4, 5]
+
+if 8 in data:
+    print(number)
+elif 4 in data:
+    print(num)
+else: 
+    print(n)
+
+for i in range(10):
+    print(i)
+
+for i in data:
+    print(i)
