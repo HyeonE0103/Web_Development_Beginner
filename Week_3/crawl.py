5317695331d6e3c583e0935bd89f3ebae4df0c44
Sohyeon <mag30102000@gmail.com>
2022-12-20 22:42:21 +0900

    3주차_서버만들기

--- Week_3/crawl.py
+++ Week_3/crawl.py
@@ -4,17 +4,31 @@
 
 import requests
 from bs4 import BeautifulSoup
+from pymongo.mongo_client import MongoClient
 
-res = requests.get("http://www.yes24.com/24/category/bestseller")
-soup = BeautifulSoup(res.text, "html.parser")
+client = MongoClient('mongodb://localhost:27017/')
+db = client.local
 
-for i in range(40):
-    idx = str(i+1)
-    if idx == "19":
-        idx = "19_line"
-    elif idx == "20":
-        idx = "20_line"
+collection = db.yes24
 
-    sstr = "#bestList > ol > li.num" + idx + " > p:nth-child(3) > a"
-    ts = soup.select_one(sstr)
-    print(ts.text)
+rows = collection.find()
+for row in rows:
+    print(row)
+
+
+# res = requests.get("http://www.yes24.com/24/category/bestseller")
+# soup = BeautifulSoup(res.text, "html.parser")
+
+# for i in range(40):
+#     idx = str(i+1)
+#     if idx == "19":
+#         idx = "19_line"
+#     elif idx == "20":
+#         idx = "20_line"
+
+#     sstr = "#bestList > ol > li.num" + idx + " > p:nth-child(3) > a"
+#     ts = soup.select_one(sstr)
+    
+#     db['yes24'].insert_one({
+#         'Title': ts.text
+#     })
